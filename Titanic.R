439163a193c1fb3dbefc4041ef7fe79eb2fa12fd
Haomin Zhang <zhanghaomin9248@gmail.com>
2024-01-20 11:06:36 +0000

    Add Machine Learning

--- Titanic.R
+++ Titanic.R
@@ -36,13 +36,13 @@ titanic_test <- titanic_data |>
 titanic_train |>
   ggplot(aes(x=Age, fill=Sex)) +
   geom_histogram(
-    position = "dodge", binwidth = 10, center = 5, na.rm=TRUE
+    position="dodge", binwidth=10, center=5, na.rm=TRUE
   ) + theme_bw()
 titanic_train |>
   ggplot(aes(x=Age, group=Survived, fill=Survived)) +
   geom_histogram(
     aes(y=after_stat(density)),
-    position="dodge", binwidth = 10, center = 5, na.rm=TRUE
+    position="dodge", binwidth=10, center=5, na.rm=TRUE
   ) + theme_bw()
 titanic_train |>
   ggplot(aes(x=Sex, group=Survived, fill=Survived)) +
@@ -60,7 +60,7 @@ titanic_train |>
   ggplot(aes(x=Fare, group=Survived, fill=Survived)) +
   geom_histogram(
     aes(y=after_stat(density)),
-    position="dodge", binwidth = 25, center = 12.5
+    position="dodge", binwidth=25, center=12.5
   ) + theme_bw()
 titanic_train |>
   ggplot(aes(x=Embarked, group=Survived, fill=Survived)) +
@@ -76,7 +76,6 @@ train_data <- titanic_train |>
   )
 test_data <- titanic_test |>
   select(
-    PassengerId,
     Age, Sex, Family,
     Pclass, Ticket, Fare, Embarked
   )
@@ -84,42 +83,44 @@ set.seed(1024)
 split_data <- initial_split(train_data)
 fold_data <- vfold_cv(train_data, strata=Survived)
 model <- rand_forest(
-  mode="classification", engine="ranger", trees=1000,
-  mtry=tune(), min_n=tune()
-)
+  mtry=tune(), trees=1000, min_n=tune()
+) |> set_engine(
+  "ranger", num.threads=8
+) |> set_mode("classification")
 recipe <- recipe(Survived ~ ., data=split_data) |>
   step_impute_knn(
-    Age, Fare, Embarked,
-    impute_with=imp_vars(
-      Sex, Family, Pclass, Ticket
-    )
+    Age,
+    impute_with=imp_vars(Sex, Family)
+  ) |>
+  step_impute_knn(
+    Fare, Embarked,
+    impute_with=imp_vars(Pclass, Ticket)
   )
 workflow <- workflow() |>
   add_recipe(recipe) |>
   add_model(model)
 grid <- grid_max_entropy(
-  mtry(c(1,7)),
-  min_n(),
-  size=20
+  finalize(mtry(), test_data), min_n(),
+  size=30
 )
 tune <- workflow |>
   tune_grid(
     resamples=fold_data,
     grid=grid,
-    metrics=metric_set(accuracy, roc_auc)
+    metrics=metric_set(accuracy, roc_auc),
+    control = control_grid(verbose=TRUE)
   )
 tune |> autoplot()
 tune |> show_best(metric="accuracy")
 tune |> show_best(metric="roc_auc")
 params <- tune |> select_best(metric="roc_auc")
-final <- workflow |>
-  finalize_workflow(params)
+final <- workflow |> finalize_workflow(params)
 final |> last_fit(split_data) |>
   collect_metrics()
 fit <- final |> fit(train_data)
 predict <- fit |> predict(test_data) |>
   rename(Survived=.pred_class)
-submission <- test_data |>
+submission <- titanic_test |>
   select(PassengerId) |>
   bind_cols(predict)
 submission |> write.csv(
